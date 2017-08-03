67c7438f0a9f3a77474a3f084876890b2c95c475
Matan Gizunterman <matan.gizunterman@arm.com>
2017-08-03 09:14:35 +0300

    First JMH draft benchmark implementation with synchrobench ported data structure.

--- src/main/java/psy/lob/saw/own/SimpleBenchmark.java
+++ src/main/java/psy/lob/saw/own/SimpleBenchmark.java
@@ -1,6 +1,9 @@
 package psy.lob.saw.own;
 
+
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
@@ -10,45 +13,108 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 
+import psy.lob.saw.synchrobench.trees.lockfree.NonBlockingTorontoBSTMap;
+
+
 public class SimpleBenchmark {
+	
+	private final static int updateRate = 50;
+	private final static boolean forcedUpdate = true;
+	private final static int initSize = 16535;
+	private final static int valueRange = 16535*2;
+	private static AtomicInteger totalAdds;
+	private static AtomicInteger totalRemoves;	
+	private static AtomicInteger totalFailures;	
+	
+    @State(Scope.Benchmark)
+    public static class GroupContext {
 
-    @State(Scope.Group)
-    public static class MyState {
-
-        @Setup(Level.Trial)
-        public void doSetup() {
-            list = new OptimisticList<Integer>();
-            rand = new Random();
-//            System.out.println("Setup - Instance List");
-        }
+    	
+    	public NonBlockingTorontoBSTMap<Integer,Integer> map;
+        private Random groupRand;
         
-        @Setup(Level.Invocation)
-        public void doRand() {
-        	a = rand.nextInt();
-            //System.out.println("Setup - Random Calc");
+    	@Setup(Level.Trial)
+        public void doSetup() {
+            map = new NonBlockingTorontoBSTMap<Integer,Integer>();
+            totalAdds = new AtomicInteger(0);
+            totalRemoves =  new AtomicInteger(0);
+            totalFailures =  new AtomicInteger(0);
+            
+            groupRand = new Random();
+            
+    		//fill
+    		for(int i=0;i<initSize;){
+    			
+    			int newInt = groupRand.nextInt(valueRange);
+    			
+    			if(map.putIfAbsent(newInt, newInt) == null)
+    				i++; //only if successful
+    		}
+            
         }
         
         @TearDown(Level.Trial)
         public void doTearDown() {
-//            System.out.println("Do TearDown");
+        	int total=totalAdds.get();
+        	total = total + totalRemoves.get();
+        	total = total + totalFailures.get();
+        	System.out.println(total);
         }
 
-        public int a = 1;
-        Random rand;
-        public OptimisticList<Integer> list;
-        public boolean res;
-    }
-
-    @Benchmark 
-    @BenchmarkMode(Mode.Throughput)
-    public void add(MyState state) {
-    	state.res = state.list.add(state.a);
-    }
+    } //end of group
     
-    @Benchmark 
+    
+    @State(Scope.Thread)
+	public static class ThreadContext {
+    	
+    	protected int numAdd;
+        protected Random rand;
+    	public boolean res;
+		protected int numRemove;
+		public int numFailures;
+        
+		
+    	@Setup(Level.Iteration)
+        public void doSetup() {
+    		numAdd = 0;
+    		rand = new Random();
+    		numRemove = 0;
+    		numFailures = 0;
+        }
+		
+        @TearDown(Level.Iteration)
+        public void doTearDown() {
+//        	totalAdds.addAndGet(numAdd);
+//        	totalRemoves.addAndGet(numRemove);
+//        	totalFailures.addAndGet(numFailures);
+        	
+        }
+	}
+    
+    @Benchmark
     @BenchmarkMode(Mode.Throughput)
-    public void remove(MyState state) {
-    	state.res = state.list.add(state.a);
+    public void run(GroupContext g, ThreadContext t) {
+    	Integer newInt = t.rand.nextInt(valueRange);
+		Integer a, b;
+		int coin = t.rand.nextInt(100);
+
+		if (coin < updateRate) { // add
+			if ((a = g.map.putIfAbsent((int) newInt, (int) newInt)) == null) {
+//				t.numAdd++;
+				totalAdds.incrementAndGet();
+			} else {
+//				t.numFailures++;
+				totalFailures.incrementAndGet();
+				
+			}
+		} else { // remove
+			if ((a = g.map.remove((int) newInt)) != null) {
+				totalRemoves.incrementAndGet();
+//				t.numRemove++;
+			} else
+				totalFailures.incrementAndGet();
+//				t.numFailures++;
+		}	
     }
 }
 	
