0c6fae2f320c83a1710927492cad8ec44bbfefea
Almog Zeltsman <almog6564@gmail.com>
2017-08-29 21:02:17 +0300

    update sym.py

--- synchro/sym.py
+++ synchro/sym.py
@@ -4,8 +4,8 @@ import csv
 from time import strftime, time, localtime
 import os
 #env
-const = []#['env', 'LD_PRELOAD=libjemalloc.so', 'numactl', '--interleave=all']
-const += ['java', '-cp', 'bin', 'contention.benchmark.Test', '-f', '1', '-d', '5000', '-s', '0', '-a', '0', '-W', '5','-gc']
+const = ['env', 'LD_PRELOAD=libjemalloc.so', 'numactl', '--interleave=all']
+const += ['java', '-cp', 'bin', 'contention.benchmark.Test', '-f', '1', '-d', '5000', '-s', '0', '-a', '0', '-W', '0']
 match_number = re.compile('-?\ *[0-9]+\.?[0-9]*(?:[Ee]\ *-?\ *[0-9]+)?')
 
 def runSync(u,i,b,t):
@@ -41,7 +41,8 @@ hashTables = ["hashtables.lockfree.NonBlockingFriendlyHashMap",
               "hashtables.lockfree.NonBlockingCliffHashMap",
               #"hashtables.lockfree.JavaHashIntSet",
               "hashtables.lockbased.LockBasedJavaHashMap",
-              "hashtables.transactional.TransactionalBasicHashSet"]
+              #"hashtables.transactional.TransactionalBasicHashSet"
+              ]
 
 linkedLists = [#"linkedlists.lockfree.NonBlockingLinkedListSetRTTI",
                "linkedlists.lockbased.LockCouplingListIntSet",
@@ -58,7 +59,10 @@ structTypesNames = ["Binary Trees", "Hash Tables", "Linked Lists", "Skip Lists"]
 upd_arg = [#0,
            50]
 name = "RunResults " + strftime("%Y.%m.%d %H-%M", localtime(time())) + ".csv"
-path = "/specific/a/home/cc/students/cs/gizunterman/mpp/synchro/results/" + name
+if os.name == 'nt':
+    path = "C:/mpp/data/mpp/synchro/WindowsResults/" + name
+else:
+    path = "/specific/a/home/cc/students/cs/zeltsman/mpp/synchro/results/" + name
 index = 0
 f = open(path, 'w+', newline='')
 writer = csv.writer(f)
@@ -80,7 +84,7 @@ for structType in enumerate(structTypes):
                         res = str(runSync(u,i,b,t))
                         results.append(res)
                         print(res,end=", ")
-                        print("")
+                    print("")
                     row = [index,structTypesNames[structType[0]],str(u)+"%",str(i),str(b),str(t)] + results
                     #print(str(index)+"%\t",row)
                     writer.writerow(row)
